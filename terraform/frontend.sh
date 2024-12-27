83bb755541019b82af62ba460647a7e6da530cb3
revanth228 <devana.reddyrevanth@gmail.com>
2024-12-27 14:17:54 -0600

    frontend-deploy

--- terraform/frontend.sh
+++ terraform/frontend.sh
@@ -4,6 +4,5 @@ environment=$2
 app_version=$3
 dnf install ansible -y
 pip3.9 install botocore boto3
--U https://github.com/Revanth228/expense-ansible-roles-expense-ansible-roles-tf.git \
-  main.yaml \ -e component=$component -e env=$environment
+ansible-pull -i localhost, -U https://github.com/Revanth228/expense-ansible-roles-expense-ansible-roles-tf.git main.yaml -e component=$component -e env=$environment
 -e appVersion=$3
